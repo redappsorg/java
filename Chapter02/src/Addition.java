170dc86f2dbb708549d309f4784062786348d7be
گروه برنامه‌نویسی برنامه‌های قرمز <62048698+redappsorg@users.noreply.github.com>
2020-05-04 17:06:23 +0430

    Adding chapter02 programs

---
+++ Chapter02/src/Addition.java
@@ -0,0 +1,20 @@
+// Addition program that inputs two numbers then displays their sum.
+import java.util.Scanner; // program uses class Scanner
+
+public class Addition {
+    // main method begins execution of Java application
+    public static void main(String[] args) {
+        // create a Scanner to obtain input from the command window
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: "); // prompt
+        int number1 = input.nextInt(); // read first number from user
+
+        System.out.print("Enter second integer: "); // prompt
+        int number2 = input.nextInt(); // read second
+
+        int sum = number1 + number2; // add numbers, then store total in sum
+
+        System.out.printf("Sum is %d%n", sum); // display sum
+    } // end method main
+} // end class Addition
