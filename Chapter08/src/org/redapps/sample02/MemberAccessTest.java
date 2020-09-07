64c0421dc26560c37592de8b948db15f9b42d199
گروه برنامه‌نویسی برنامه‌های قرمز <62048698+redappsorg@users.noreply.github.com>
2020-09-07 11:17:48 +0430

    Add files via upload

---
+++ Chapter08/src/org/redapps/sample02/MemberAccessTest.java
@@ -0,0 +1,16 @@
+// package org.redapps.sample02;
+//
+//// MemberAccessTest.java
+//// Private members of class Time1 are not accessible.
+//
+// import org.redapps.sample01.Time1;
+//
+// public class MemberAccessTest {
+//    public static void main(String[] args) {
+//        Time1 time = new Time1(); // create and initialize Time1 object
+//
+//        time.hour = 7; // error: hour has private access in Time1
+//        time.minute = 15; // error: minute has private access Time1
+//        time.second = 30; // error: second has private access in Time1
+//    }
+//}
