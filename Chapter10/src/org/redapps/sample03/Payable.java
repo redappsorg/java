63e4bcf1349796759268c2b2edd0af3e49e2389c
گروه برنامه‌نویسی برنامه‌های قرمز <62048698+redappsorg@users.noreply.github.com>
2020-09-07 11:23:16 +0430

    Add files via upload

---
+++ Chapter10/src/org/redapps/sample03/Payable.java
@@ -0,0 +1,8 @@
+package org.redapps.sample03;
+
+// Payable.java
+// Payable interface declaration.
+
+public interface Payable {
+    public abstract double getPaymentAmount(); // no implementation
+}
